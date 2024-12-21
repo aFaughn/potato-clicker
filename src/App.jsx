4086798da74b2283305d854de801d946c1133569
aFaughn <allenfaughn@gmail.com>
2024-12-21 17:58:52 -0600

    added farmers

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { useState, useEffect } from 'react'
 import spud from './assets/spud.png'
 import cursor from './assets/cursor.png'
+import farmer from './assets/farmer.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   const [cursors, setCursors] = useState(0)
+  const [farmers, setFarmers] = useState(0)
   const [clock, setClock] = useState(111)
 
   const handleSpudClick = () => {
     setCount(count + 1)
   }
 
-  const buy = (what) => {
-    if (what === 'cursor') {
+  const buy = (item) => {
+    if (item === 'cursor') {
       setCursors(cursors + 1)
+    }
+    if (item === 'farmer') {
+      setFarmers(farmers + 1)
     }
   }
 
@@ -24,14 +29,17 @@ function App() {
       setCount(count + cursors * 0.3)
     },clock)
     return () => clearTimeout(timeout)
-  },[count,cursors])
+  },[count,cursors,farmers])
 
   return (
     <>
       <div id='currentBank'>Potatoes: {Math.round(count)}</div>
       <div id='store-container'>
-        <div id='buy-cursors' onClick={() => buy('cursor')} Title={"0.1 PpS/Each"}>
+        <div id='buy-cursors' onClick={() => buy('cursor')} Title={`0.1 PpS/Each | Current: ${(cursors * 0.1).toFixed(2)} PpS`}>
           <img id='cursor-img' src={cursor}></img><p>Cursors: {cursors}</p>
+        </div>
+        <div id='buy-cursors' onClick={() => buy('farmer')} Title={`1 PpS/Each | Current: ${(farmers * 1).toFixed(2)} PpS`}>
+          <img id='cursor-img' src={farmer}></img><p>Farmers: {farmers}</p>
         </div>
       </div>
       <div id='upgrades-container'>
