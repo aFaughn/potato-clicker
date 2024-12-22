46f97a41740abb144151550777726451bba3f08a
aFaughn <allenfaughn@gmail.com>
2024-12-22 16:24:05 -0600

    adding upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,15 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
   const [cursors, setCursors] = useState(0)
+  const [cursorMult, setCursorMult] = useState(0.01)
+  const [cursorCost, setCursorCost] = useState(10)
+
   const [farmers, setFarmers] = useState(0)
+  const [farmerMult, setFarmerMult] = useState(0.1)
+  const [farmerCost, setFarmerCost] = useState(100)
+
   const [clock, setClock] = useState(111)
 
   const handleSpudClick = () => {
@@ -16,17 +23,25 @@ function App() {
 
   const buy = (item) => {
     if (item === 'cursor') {
-      setCursors(cursors + 1)
+      if (count >= cursorCost) {
+        setCursors(cursors + 1)
+        setCount(count - cursorCost)
+        setCursorCost(Math.round(cursorCost * 1.10))
+      }
     }
     if (item === 'farmer') {
-      setFarmers(farmers + 1)
+      if (count >= farmerCost) {
+        setFarmers(farmers + 1)
+        setCount(count - farmerCost)
+        setFarmerCost(Math.round(farmerCost * 1.12))
+      }
     }
   }
 
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setCount(count + cursors * 0.3)
+      setCount(count + (cursors * cursorMult) + (farmers * farmerMult))
     },clock)
     return () => clearTimeout(timeout)
   },[count,cursors,farmers])
@@ -36,15 +51,20 @@ function App() {
       <div id='currentBank'>Potatoes: {Math.round(count)}</div>
       <div id='store-container'>
         <div id='buy-cursors' onClick={() => buy('cursor')} Title={`0.1 PpS/Each | Current: ${(cursors * 0.1).toFixed(2)} PpS`}>
-          <img id='cursor-img' src={cursor}></img><p>Cursors: {cursors}</p>
+          <img id='cursor-img' src={cursor}></img><p>Cursors: {cursors}     Price: {cursorCost}</p>
         </div>
         <div id='buy-cursors' onClick={() => buy('farmer')} Title={`1 PpS/Each | Current: ${(farmers * 1).toFixed(2)} PpS`}>
-          <img id='cursor-img' src={farmer}></img><p>Farmers: {farmers}</p>
+          <img id='cursor-img' src={farmer}></img><p>Farmers: {farmers}     Price: {farmerCost}</p>
         </div>
       </div>
       <div id='upgrades-container'>
-        Upgrades
-
+        <div className='upgrade' id='t1'>
+          <img className='upgradeIcon' src={cursor}></img>
+        </div>
+        <div className='upgrade' id='t1'>
+          <img className='upgradeIcon' src={farmer}></img>
+        </div>
+      
       </div>
       <div id='spud'><img 
 
